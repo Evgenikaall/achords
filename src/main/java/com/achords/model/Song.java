084e7afb01d91491475ed6fcc0127a76b1fbbec2
evgenikaall <evgenikaall@gmail.com>
2021-01-06 01:39:43 +0200

    add tuning service and rest tuning

--- src/main/java/com/achords/model/Song.java
+++ src/main/java/com/achords/model/Song.java
@@ -3,9 +3,7 @@ package com.achords.model;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Entity
 @Data
@@ -14,25 +12,30 @@ public class Song {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer songId;
 
     private String songName;
 
     private Date postDate;
 
     @ManyToOne
+    @JoinColumn(name = "tuning",insertable = false, updatable = false)
     private Tuning tuning;
 
     @ManyToOne
+    @JoinColumn(name = "difficult_id",insertable = false, updatable = false)
     private DifficultLevel difficultLevel;
 
     @ManyToOne
+    @JoinColumn(name = "author_id",insertable = false, updatable = false)
     private Author author;
 
     @ManyToOne
+    @JoinColumn(name = "section_type_id",insertable = false, updatable = false)
     private SectionType sectionType;
 
-    @ManyToMany
+    @OneToMany
+    @JoinColumn(name = "chord_name",insertable = false, updatable = false)
     private List<Chords> chordsList = new ArrayList<>();
 
     @ManyToMany
@@ -42,6 +45,7 @@ public class Song {
     private List<Language> languagesList = new ArrayList<>();
 
     @ManyToMany
+
     private List<StrummingPattern> strummingPatternList = new ArrayList<>();
 
     //text song
